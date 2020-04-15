1f3bd3bab70d835fcfee023bcd1d3989ec64f6cc
Tomek <owerko@gmail.com>
2020-04-15 22:56:48 +0200

    module jednorodny LSF H

--- jednorodny/main.py
+++ jednorodny/main.py
@@ -1,14 +1,56 @@
 from jednorodny.point import Point
 from jednorodny.epoch import Epoch
+import math
+import numpy as np
 
-# data = []
+
+def radius(perimeter):
+    return perimeter / (2 * math.pi)
 
 
-with open('data.txt', 'r') as f:
-    e = Epoch('pomiar')
-    for line in f:
-        nr, cr, dx, dy, dz = line.split()
-        point = Point(nr, cr, dx, dy, dz)
-        e.add_point(point)
+def azimuth(radius, perimeter):
+    return perimeter * 400 / (2 * math.pi * radius)
 
-e.points_list()
+
+def A(epoch):
+    lst = []
+    for i in range(len(epoch.points)):
+        lst.append((1, epoch.points[i].y, -1 * epoch.points[i].x))
+    return np.array(lst)
+
+
+def L(epoch):
+    lst = []
+    for i in range(len(epoch.points)):
+        lst.append(float(epoch.points[i].dz))
+    return np.array(lst)
+
+
+def lsf(A, L):
+    At = A.transpose()
+    AtA = np.matmul(At, A)
+    AtA_1 = np.linalg.inv(AtA)
+    AtL = np.matmul(At, L)
+    return np.matmul(AtA_1, AtL)
+
+
+if __name__ == '__main__':
+    perimeter = float(input('Podaj obwod fundamentu: '))
+    r = radius(perimeter)
+    with open('data.txt', 'r') as f:
+        epoka = Epoch('pomiar')
+        for line in f:
+            nr, pm, dx, dy, dz = line.split()
+            az = azimuth(r, float(pm))
+            x = r * math.cos(az * math.pi / 200)
+            y = r * math.sin(az * math.pi / 200)
+            point = Point(nr, pm, dx, dy, dz, x, y)
+            epoka.add_point(point)
+            # dim += 1
+
+    print(f'Macierz A ma postać:')
+    print(A(epoka))
+    print(f'Macierz L ma postać:')
+    print(L(epoka))
+    X = lsf(A(epoka), L(epoka))
+    print(f'Osiadanie wynosi {X[0]:.2f} mm, obrot wokol osi X {X[1]:.3f} mm/m, obrot wokol osi Y {X[2]:.3f} mm/m')
--- jednorodny/point.py
+++ jednorodny/point.py
@@ -1,15 +1,17 @@
 class Point:
-    def __init__(self, nr, circuit, dx, dy, dz):
+    def __init__(self, nr, perimeter, dx, dy, dz, x=0, y=0):
         self.nr = nr
-        self.circuit = circuit
+        self.perimeter = perimeter
         self.dx = dx
         self.dy = dy
         self.dz = dz
+        self.x = x
+        self.y = y
 
     def __str__(self):
-        return f'{self.nr} {self.circuit} {self.dx} {self.dy} {self.dz}'
+        return f'{self.nr} {self.dx} {self.dy} {self.dz} {self.x} {self.y}'
 
     def __eq__(self, o: object) -> bool:
         if not isinstance(o, Point):
             return False
-        return self.nr == o.nr and self.circuit == o.circuit and self.dx == o.dx and self.dy == o.dy and self.dz == o.dz
+        return self.nr == o.nr and self.perimeter == o.perimeter and self.dx == o.dx and self.dy == o.dy and self.dz == o.dz
